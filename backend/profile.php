27c1bf3a4a78dc21bfd1a15c2b371fe6616b3458
shourab <shourab.cit.bd@gmail.com>
2024-01-25 16:28:10 +0600

    2302

--- backend/profile.php
+++ backend/profile.php
@@ -18,22 +18,25 @@ include_once "./backendLayout/header.php";
                     <div class="card-header">Profile info</div>
                     <div class="card-body">
 
-                        <div class="row">
-
-                            <div class="col-lg-6">
-
-                                <img src="https://api.dicebear.com/7.x/initials/svg?seed=<?= ucwords($_SESSION['auth']['fname']) ?? '' ?>" alt="">
-
+                        <form enctype="multipart/form-data" action="../controller/profileUpdate.php" method="POST">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <label class="d-block" for="profileInput">
+                                        <img style="max-width:100% ;" class="profileImage" src="https://api.dicebear.com/7.x/initials/svg?seed=<?= ucwords($_SESSION['auth']['fname']) ?? '' ?>" alt="">
+                                    </label>
+                                    <input name="profileImage" class="d-none" type="file" id="profileInput">
+                                    <span class="text-danger">
+                                        <?= $_SESSION['errors']['profileImage'] ?? '' ?>
+                                    </span>
+                                </div>
+                                <div class="col-lg-6">
+                                    <input name="fname" value="<?= $_SESSION['auth']['fname'] ?? '' ?>" placeholder="First name" type="text" class=" my-2 form-control">
+                                    <input name="lname" value="<?= $_SESSION['auth']['lname'] ?? '' ?>"  placeholder="Last name" type="text" class=" my-2 form-control">
+                                    <input name="email" value="<?= $_SESSION['auth']['email'] ?? '' ?>"  placeholder="Email" type="text" class=" my-2 form-control">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
                             </div>
-                            <div class="col-lg-6">
-                                <input placeholder="First name" type="text" class=" my-2 form-control">
-                                <input placeholder="Last name" type="text" class=" my-2 form-control">
-                                <input placeholder="Email" type="text" class=" my-2 form-control">
-                                <button class="btn btn-primary">Submit</button>
-                            </div>
-
-
-                        </div>
+                        </form>
 
 
                     </div>
@@ -45,11 +48,11 @@ include_once "./backendLayout/header.php";
                     <div class="card-body">
                         <form action="">
 
-                        <input placeholder="Old Password" type="text" class="form-control">
-                        <input placeholder="New Password" type="text" class="form-control">
-                        <input placeholder="Confirm Password" type="text" class="form-control">
+                            <input placeholder="Old Password" type="text" class="form-control">
+                            <input placeholder="New Password" type="text" class="form-control">
+                            <input placeholder="Confirm Password" type="text" class="form-control">
 
-                        <button class="btn btn-primary">Update Password</button>
+                            <button class="btn btn-primary">Update Password</button>
 
                         </form>
                     </div>
@@ -69,4 +72,18 @@ include_once "./backendLayout/header.php";
 
 include_once "./backendLayout/footer.php";
 
+unset($_SESSION['errors']);
+
 ?>
+<script>
+    let profileInput = document.querySelector("#profileInput")
+    let profileImage = document.querySelector('.profileImage')
+
+    function updatePreviewImage(event) {
+        let url = URL.createObjectURL(event.target.files[0])
+        profileImage.src = url;
+
+    }
+
+    profileInput.addEventListener('change', updatePreviewImage)
+</script>
---
+++ controller/profileUpdate.php
@@ -0,0 +1,71 @@
+<?php
+include "../database/env.php";
+//* DATA COLLECT
+session_start();
+$fname = $_REQUEST['fname'];
+$lname = $_REQUEST['lname'];
+$email = $_REQUEST['email'];
+$profileImage = $_FILES['profileImage'];
+$extension = pathinfo($profileImage['name'])['extension'];
+$accecptedTypes = ['jpg', 'png'];
+$userId = $_SESSION['auth']['id'];
+
+// echo "<pre>";
+// print_r($profileImage);
+// echo "</pre>";
+// exit();
+
+
+$errors = [];
+
+//* VALIDATION
+// if($profileImage['size'] == 0) {
+//     $errors['profileImage'] = 'Please select an image';
+// } else if(!in_array($extension, $accecptedTypes)){
+//     $errors['profileImage'] = 'Please select an image with the extension of jpg or png';
+// }
+
+
+
+//* IF ERRORS FOUND
+if (count($errors) > 0) {
+    //* REDIRECT BACK
+    $_SESSION['errors'] = $errors;
+    header("location: ../backend/profile.php");
+}
+
+//* IF NO ERROR FOUND
+
+else {
+    //* IF UPLOAD FOLDER NOT FOUND
+    $path = '../uploads';
+    if (!file_exists($path)) {
+        mkdir($path);
+    }
+
+
+    //*  FILE UPLOAD
+
+    if ($profileImage['size'] > 0) {
+
+        $fileName = 'user-' . uniqid() . ".$extension";
+        $from = $profileImage['tmp_name'];
+        $to = $path . "/$fileName";
+
+        $isUploaded = move_uploaded_file($from, $to);
+        $query = "UPDATE users SET fname='$fname',lname='$lname',email='$email',profile_img='$fileName' WHERE id = '$userId'";
+        $res  = mysqli_query($conn, $query);
+    } else {
+
+        $query = "UPDATE users SET fname='$fname',lname='$lname',email='$email' WHERE id = '$userId'";
+        $res  = mysqli_query($conn, $query);
+    }
+
+    if($res){
+        $_SESSION['auth']['fname'] = $fname;
+        $_SESSION['auth']['lname'] = $lname;
+        $_SESSION['auth']['email'] = $email;
+    }
+
+
+}
